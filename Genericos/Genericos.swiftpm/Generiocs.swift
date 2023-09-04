0f4ee90723e9b195f497bfa2605079bbc1cd3813
Ana Reguera <amarebu1999@gmail.com>
2023-09-04 08:07:11 +0200

    Añadido explicación sobre genéricos

---
+++ Genericos/Genericos.swiftpm/Generiocs.swift
@@ -0,0 +1,8 @@
+//
+//  Generiocs.swift
+//  Genericos
+//
+//  Created by Reguera Bueno Ana María on 14/8/23.
+//
+
+import Foundation
--- Genericos/basicosSwift.playground/Pages/Genericos.xcplaygroundpage/Contents.swift
+++ Genericos/basicosSwift.playground/Pages/Genericos.xcplaygroundpage/Contents.swift
@@ -9,5 +9,51 @@ import Foundation
     - No repetir código
  */
 
+class IntLifoQueue {
+    var elements: [Int] = []
+    
+    func push(_ element: Int) {
+        elements.append(element)
+    }
+    
+    func pop() -> Int? {
+        if elements.count <= 0 {
+            return nil
+        }
+        let lastElement = elements.removeLast()
+        return lastElement
+    }
+}
 
+let intengerImp = IntLifoQueue()
+intengerImp.push(4)
+intengerImp.push(5)
+
+// print(String(intengerImp.pop() ?? -1))
+// print(String(intengerImp.pop() ?? -1))
+// print(String(intengerImp.pop() ?? -1))
+
+/*
+ Genérico: Clase, estructura, enumerado, función o método, donde uno o varios de los tipos de datos que maneja no están definidos en el momento de definir el código. El tipo  no definido toma nombre de genérico.
+ */
+
+class LifoQueue<StoredType> {
+    var elements: [StoredType] = []
+    
+    func push(_ element: StoredType) {
+        elements.append(element)
+    }
+    func pop() -> StoredType? {
+        if elements.count <= 0 {
+            return nil
+        }
+        let lastElemet = elements.removeLast()
+        return lastElemet
+    }
+}
+
+let integerLifo: LifoQueue<Int> = LifoQueue()
+integerLifo.push(6)
+integerLifo.push(7)
+print("This is an example: ", integerLifo.elements)
 //: [Next](@next)
