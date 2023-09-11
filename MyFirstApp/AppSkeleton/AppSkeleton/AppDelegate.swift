bb7c19a606dd693e297ae6937dc5722638382c22
Ana María Reguera Bueno <anama.reguera@gruposantander.com>
2023-09-11 13:06:05 +0200

    Actualización

--- MyFirstApp/AppSkeleton/AppSkeleton/AppDelegate.swift
+++ MyFirstApp/AppSkeleton/AppSkeleton/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate { // Protocolo que recibe 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // LA aplicación ha finalizado
+        print("La aplicación se ha creado")
         return true
     }
 
